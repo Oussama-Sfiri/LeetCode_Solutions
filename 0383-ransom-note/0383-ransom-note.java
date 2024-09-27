13b92c75564c05bd0ac5149258e42552d02482ed
Oussama Sfiri <125862340+Oussama-Sfiri@users.noreply.github.com>
2024-09-27 12:57:15 +0100

    Time: 10 ms (58.43%), Space: 45.4 MB (12.03%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,21 +3,36 @@ import java.util.Set;
 
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Hashtable<Character, Integer> ht1 = new Hashtable<>();
-        Hashtable<Character, Integer> ht2 = new Hashtable<>();
-        for (char c : magazine.toCharArray()){
-            ht1.put(c, ht1.getOrDefault(c, 0) + 1);
+                HashMap<Character, Integer> magaHash = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            magaHash.put(c, magaHash.getOrDefault(c, 0) + 1);
         }
-        for (char c : ransomNote.toCharArray()){
-            ht2.put(c, ht2.getOrDefault(c, 0) + 1);
-        }
-        Set<Character> setOfKeys = ht2.keySet();
-        for (char key : setOfKeys) {
-            if (ht1.getOrDefault(key, 0) < ht2.get(key)) {
+
+        for (char c : ransomNote.toCharArray()) {
+            if (!magaHash.containsKey(c) || magaHash.get(c) <= 0) {
                 return false;
             }
+            magaHash.put(c, magaHash.get(c) - 1);
         }
-        
+
         return true;
+
+        // Hashtable<Character, Integer> ht1 = new Hashtable<>();
+        // Hashtable<Character, Integer> ht2 = new Hashtable<>();
+        // for (char c : magazine.toCharArray()){
+        //     ht1.put(c, ht1.getOrDefault(c, 0) + 1);
+        // }
+        // for (char c : ransomNote.toCharArray()){
+        //     ht2.put(c, ht2.getOrDefault(c, 0) + 1);
+        // }
+        // Set<Character> setOfKeys = ht2.keySet();
+        // for (char key : setOfKeys) {
+        //     if (ht1.getOrDefault(key, 0) < ht2.get(key)) {
+        //         return false;
+        //     }
+        // }
+        
+        // return true;
     }
 }
