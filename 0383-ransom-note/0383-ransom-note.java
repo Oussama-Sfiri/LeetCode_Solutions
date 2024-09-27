92b9f92d9f870f84f987cc96ef94034f0709eaf1
Oussama Sfiri <125862340+Oussama-Sfiri@users.noreply.github.com>
2024-09-27 11:55:38 +0100

    Time: 20 ms (9.19%), Space: 45.3 MB (20.21%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,18 +3,16 @@ import java.util.Set;
 
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Hashtable<String, Integer> ht1 = new Hashtable<>();
-        Hashtable<String, Integer> ht2 = new Hashtable<>();
+        Hashtable<Character, Integer> ht1 = new Hashtable<>();
+        Hashtable<Character, Integer> ht2 = new Hashtable<>();
         for (char c : magazine.toCharArray()){
-            String key = String.valueOf(c);
-            ht1.put(key, ht1.getOrDefault(key, 0) + 1);
+            ht1.put(c, ht1.getOrDefault(c, 0) + 1);
         }
         for (char c : ransomNote.toCharArray()){
-            String key = String.valueOf(c);
-            ht2.put(key, ht2.getOrDefault(key, 0) + 1);
+            ht2.put(c, ht2.getOrDefault(c, 0) + 1);
         }
-        Set<String> setOfKeys = ht2.keySet();
-        for (String key : setOfKeys) {
+        Set<Character> setOfKeys = ht2.keySet();
+        for (char key : setOfKeys) {
             if (ht1.getOrDefault(key, 0) < ht2.get(key)) {
                 return false;
             }
